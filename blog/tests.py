862e9251bfb0a73d8c742b1269bfd70c14d7db3f
Aydar2007 <brawlball22@mail.ru>
2023-06-22 13:25:50 +0600

    LESSON_5

--- blog/tests.py
+++ blog/tests.py
@@ -1,39 +1,36 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from blog.models import Post
+
 
 class BlogTestCase(TestCase):
     def setUp(self):
         self.client = Client()
+        self.post = Post.objects.create(title="test", content="test")
 
     def test_index(self):
         # reverse по имени эндпоинта "index-page" возвращает строку http://127.0.0.1:8000/
         response = self.client.get(reverse("index-page"))
-
+        self.assertTemplateUsed(response, "blog/index.html")
         self.assertEqual(response.status_code, 200)
 
     def test_get_about(self):
         response = self.client.get(reverse("about-page"))
-
+        self.assertTemplateUsed(response, "blog/about.html")
         self.assertEqual(response.status_code, 200)
 
     def test_get_contacts(self):
         response = self.client.get(reverse("contacts-page"))
-
+        self.assertTemplateUsed(response, "blog/contacts.html")
         self.assertEqual(response.status_code, 200)
 
-
-    def test_contacts_rending(self):
-        response = self.client.get(reverse("contacts-page"))
-        self.assertTemplateUsed(response,"blog/contacts.html")
+    def test_post_update(self):
+        response = self.client.get(reverse("post-update", args=(1,)))
+        self.assertTemplateUsed(response, "blog/post_update.html")
         self.assertEqual(response.status_code, 200)
 
-    def test_about_rending(self):
-        response = self.client.get(reverse("about-page"))
-        self.assertTemplateUsed(response,'blog/about.html')
-        self.assertEqual(response.status_code, 200)
-    
-    def test_index_rending(self):
-        response = self.client.get(reverse("index-page"))
-        self.assertTemplateUsed(response,"blog/index.html")
+    def test_post_delete(self):
+        response = self.client.get(reverse("post-delete", args=(1,)))
+        self.assertTemplateUsed(response, "blog/post_delete.html")
         self.assertEqual(response.status_code, 200)
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from blog import views
 
 urlpatterns = [
-    path("", views.index, name="index-page"),
-    path("about/", views.get_about, name="about-page"),
+    path("", views.IndexListView.as_view(), name="index-page"),
+    path("about/", TemplateView.as_view(template_name="blog/about.html"), name="about-page"),
     path("contacts/", views.get_contacts, name="contacts-page"),
+    path("post/<int:pk>", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/update/<int:pk>", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/delete/<int:pk>", views.PostDeleteView.as_view(), name="post-delete"),
+    path("post/create", views.PostCreateView.as_view(), name="post-create"),
 ]
